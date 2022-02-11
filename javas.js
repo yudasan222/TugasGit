bb4afe94be1a145e824ef26b30cf9c01e4a10536
unknown <ryuda1014@gmail.com>
2022-02-11 16:44:07 +0700

    tambah file javas.js

---
+++ javas.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default javas native() {
+  return (
+    <View>
+      <Text>ini komponen fungsi di react native</Text>
+    </View>
+  )
+}
